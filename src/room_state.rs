3ad741576c8d24e71b8a979b5a33b621ccec4c16
TlMegalovania <tlmegalovania@qq.com>
2022-08-14 18:16:38 +0800

    feat: details to info
    Signed-off-by: TlMegalovania <tlmegalovania@qq.com>

--- src/room_state.rs
+++ src/room_state.rs
@@ -1,3 +1,3 @@
-const AVAILABLE: i32 = 0;
-const FULL: i32 = 1;
-const IN_GAME: i32 = 2;
+pub(crate) const AVAILABLE: i32 = 0;
+pub(crate) const FULL: i32 = 1;
+pub(crate) const IN_GAME: i32 = 2;
--- src/types.rs
+++ src/types.rs
@@ -1,10 +1,12 @@
-struct RoomInfo {
+use crate::room_state;
+
+pub(crate) struct RoomInfo {
     pub Player1: String,
     pub Player2: String,
     pub State: i32,
 }
 
-struct RoomDetail {
+pub(crate) struct RoomDetail {
     pub Player1: String,
     pub Player2: String,
     pub P1Ready: bool,
@@ -15,4 +17,24 @@ pub(crate) struct BoardInfo {
     pub Board: Vec<i32>,
     pub Turn: i32,
     pub Result: i32,
+}
+
+pub(crate) fn roomDetailsToInfo(rd: &[RoomDetail]) -> Vec<RoomInfo> {
+    let mut values = Vec::with_capacity(rd.len());
+    for info in rd {
+        let state;
+        if info.P1Ready && info.P2Ready {
+            state = room_state::IN_GAME
+        } else if info.Player1.is_empty() || info.Player2.is_empty() {
+            state = room_state::AVAILABLE
+        } else {
+            state = room_state::FULL
+        }
+        values.push(RoomInfo {
+            Player1: info.Player1.clone(),
+            Player2: info.Player2.clone(),
+            State: state,
+        })
+    }
+    return values;
 }
